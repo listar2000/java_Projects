cfa3200fd50d38f784178ee4b795961e3cff9f3f
listar2000 <35262801+listar2000@users.noreply.github.com>
2018-03-15 22:10:45 +0800

    Major update in its functionalities

--- sortingProject/PhoneBook_menuDriven.java
+++ sortingProject/PhoneBook_menuDriven.java
@@ -18,12 +18,16 @@ public class PhoneBook_menuDriven {
 		System.out.println("***输入2进入删除联系人界面");
 		System.out.println("***输入3显示所有的联系人");
 		System.out.println("***输入4推出系统（结束）");
+		System.out.println("***输入5保存联系人名单到指定txt文档");
 		
 		while (true) 
 		{
 			int option = reader.nextInt();
 			
-			if (option == 4) System.exit(0);
+			if (option == 4) { 
+				phoneBook.autoSaveBeforeExit();
+				System.exit(0);
+			}
 			
 			switch (option) {
 			case 1:
@@ -35,6 +39,9 @@ public class PhoneBook_menuDriven {
 			case 3:
 				printFunction();
 				break;
+			case 5:
+				saveFunction();
+				break;
 			default:
 				System.out.println("请输入有效的数字指令！");
 				break;
@@ -42,6 +49,13 @@ public class PhoneBook_menuDriven {
 		}
 	}
 	
+	public void saveFunction() {
+		System.out.println("开始保存！！");
+		System.out.println("注意IDE有可能遮挡了文件选择框，请最小化IDE");
+		phoneBook.userCreateFile();
+		System.out.println("O(∩_∩)O 完成！可以输入下一个指令了");
+	}
+
 	public void addFunction() 
 	{
 		System.out.println("***欢迎使用功能1，请先输入添加联系人的姓名***");
