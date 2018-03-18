37cd8558b7716b70f24183797d0e55dc37bf64f9
listar2000 <35262801+listar2000@users.noreply.github.com>
2018-03-19 00:47:54 +0800

    Add a few questions

--- sortingProject/PhoneBook_menuDriven.java
+++ sortingProject/PhoneBook_menuDriven.java
@@ -2,6 +2,10 @@ package sortingProject;
 
 import java.util.Scanner;
 
+/**
+ * @author listar2000
+ * @project APCS Sorting Project
+ */
 public class PhoneBook_menuDriven {
 	private PhoneBook phoneBook;
 	private Scanner reader;
@@ -17,14 +21,18 @@ public class PhoneBook_menuDriven {
 		System.out.println("***输入1进入添加联系人界面");
 		System.out.println("***输入2进入删除联系人界面");
 		System.out.println("***输入3显示所有的联系人");
-		System.out.println("***输入4推出系统（结束）");
-		System.out.println("***输入5保存联系人名单到指定txt文档");
+		System.out.println("***输入4保存联系人名单到指定txt文档");
+		System.out.println("***输入5查询联系人（姓名查询）");
+		System.out.println("***输入6将联系人以名字顺序排序");
+		System.out.println("***输入7将联系人以电话号码排序");
+		System.out.println("***输入8退出系统（结束）");
+		System.out.println("*************************************");
 		
 		while (true) 
 		{
 			int option = reader.nextInt();
 			
-			if (option == 4) { 
+			if (option == 8) { 
 				phoneBook.autoSaveBeforeExit();
 				System.exit(0);
 			}
@@ -39,24 +47,41 @@ public class PhoneBook_menuDriven {
 			case 3:
 				printFunction();
 				break;
-			case 5:
+			case 4:
 				saveFunction();
 				break;
+			case 5:
+				searchFunction();
+				break;
+			case 6:
+				nameSearchFunction();
+			case 7:
+				phoneSearchFunction();
 			default:
 				System.out.println("请输入有效的数字指令！");
 				break;
 			}
+			System.out.println("***😄可以进行下一个操作了***");
 		}
 	}
 	
-	public void saveFunction() {
+	private void searchFunction() 
+	{
+		System.out.println("请输入查询人姓名");
+		
+		Person searchPerson = phoneBook.getPerson(reader.next());
+		
+		if (searchPerson == null) System.out.println("^^^查无此人^^^");
+		else System.out.println(searchPerson);
+	}
+
+	private void saveFunction() {
 		System.out.println("开始保存！！");
 		System.out.println("注意IDE有可能遮挡了文件选择框，请最小化IDE");
 		phoneBook.userCreateFile();
-		System.out.println("O(∩_∩)O 完成！可以输入下一个指令了");
 	}
 
-	public void addFunction() 
+	private void addFunction() 
 	{
 		System.out.println("***欢迎使用功能1，请先输入添加联系人的姓名***");
 		String newName = reader.next();
@@ -77,11 +102,9 @@ public class PhoneBook_menuDriven {
 		String newZip = reader.next();
 		
 		phoneBook.addPerson(new Person(newName, newPhone, newZip));
-		
-		System.out.println("O(∩_∩)O 完成！可以输入下一个指令了");
 	}
 	
-	public void deleteFunction() 
+	private void deleteFunction() 
 	{
 		System.out.println("***欢迎使用功能2，请先输入想要删除的联系人的姓名***");
 		String deleteName = reader.next();
@@ -91,16 +114,28 @@ public class PhoneBook_menuDriven {
 		else {
 			System.out.println("系统中查无此人");
 		}
-		System.out.println("O(∩_∩)O 完成！可以输入下一个指令了");
 	}
 	
-	public void printFunction()
+	private void printFunction()
 	{
-		System.out.println("***欢迎使用功能3，以下为该联系人册输出的结果***");
+		System.out.println("***以下为该联系人册输出的结果***");
 		phoneBook.printBook();
-		
+
 		System.out.println();
-		System.out.println("O(∩_∩)O 完成！可以输入下一个指令了");
+	}
+	
+	private void nameSearchFunction() 
+	{
+		phoneBook.sortByName();
+		System.out.println("***排序成功***");
+		printFunction();
+	}
+	
+	private void phoneSearchFunction() 
+	{
+		phoneBook.sortByNum();
+		System.out.println("***排序成功***");
+		printFunction();
 	}
 	
 	public static void main(String[] args) {
