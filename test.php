81ef0d74d1a803f114df15a9fe17208e9f9dd879
listar2000 <35262801+listar2000@users.noreply.github.com>
2018-04-23 18:55:23 +0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,120 @@
+<?php
+define("CURL_TIMEOUT", 20);
+define("URL", "https://openapi.youdao.com/api");
+define("APP_KEY", "77957594e8cae0f8"); //替换为您的应用ID
+define("SEC_KEY", "0gA9t1gdzzcBTV0Bm1Oc4Wp0sGI4S4v1");//替换为您的密钥
+
+//翻译入口
+function translate($query, $from, $to)
+{
+    $args = array(
+        'q' => $query,
+        'appKey' => APP_KEY,
+        'salt' => rand(10000,99999),
+        'from' => $from,
+        'to' => $to,
+    );
+    $args['sign'] = buildSign(APP_KEY, $query, $args['salt'], SEC_KEY);
+    $ret = call(URL, $args);
+    echo $ret;
+    $ret = json_decode($ret, true);
+    return $ret;
+}
+
+//加密
+function buildSign($appKey, $query, $salt, $secKey)
+{/*{{{*/
+    $str = $appKey . $query . $salt . $secKey;
+    $ret = md5($str);
+    return $ret;
+}/*}}}*/
+
+//发起网络请求
+function call($url, $args=null, $method="post", $testflag = 0, $timeout = CURL_TIMEOUT, $headers=array())
+{/*{{{*/
+    $ret = false;
+    $i = 0;
+    while($ret === false)
+    {
+        if($i > 1)
+            break;
+        if($i > 0)
+        {
+            sleep(1);
+        }
+        $ret = callOnce($url, $args, $method, false, $timeout, $headers);
+        $i++;
+    }
+    return $ret;
+}/*}}}*/
+
+function callOnce($url, $args=null, $method="post", $withCookie = false, $timeout = CURL_TIMEOUT, $headers=array())
+{/*{{{*/
+    $ch = curl_init();
+    if($method == "post")
+    {
+        $data = convert($args);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+        curl_setopt($ch, CURLOPT_POST, 1);
+    }
+    else
+    {
+        $data = convert($args);
+        if($data)
+        {
+            if(stripos($url, "?") > 0)
+            {
+                $url .= "&$data";
+            }
+            else
+            {
+                $url .= "?$data";
+            }
+        }
+    }
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    if(!empty($headers))
+    {
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    }
+    if($withCookie)
+    {
+        curl_setopt($ch, CURLOPT_COOKIEJAR, $_COOKIE);
+    }
+    $r = curl_exec($ch);
+    curl_close($ch);
+    return $r;
+}/*}}}*/
+
+function convert(&$args)
+{
+    $data = '';
+    if (is_array($args))
+    {
+        foreach ($args as $key=>$val)
+        {
+            if (is_array($val))
+            {
+                foreach ($val as $k=>$v)
+                {
+                    $data .= $key.'['.$k.']='.rawurlencode($v).'&';
+                }
+            }
+            else
+            {
+                $data .="$key=".rawurlencode($val)."&";
+            }
+        }
+        return trim($data, "&");
+    }
+    return $args;
+}
+
+$word = $_GET['word'];
+echo $word;
+
+//调用翻译
+translate("$word","EN","zh-CHS");
+?>
